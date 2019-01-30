b52ae166efa1ce6a8e6290c6d9d8999a22586690
Prashanth MJ <prashanth000mj@gmail.com>
2019-01-30 21:15:40 +0530

    added nginx setup statements

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 !#/bin/shell
-echo "Hello Prashanth"
+echo "Hello Team"
+
+echo " ----- Installing NGINX -----"
+sudo apt update
+sudo apt install nginx
+sudo ufw allow 'Nginx HTTP'
+echo " ----- NGINX is Up and running-----"
+echo "Commands to look at NGINX: systemctl status/stop/start nginx"
