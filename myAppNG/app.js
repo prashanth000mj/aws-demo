7951df750047d99262078295e8aeb70a5bb8a3f0
root <root@ip-172-31-9-103.ap-south-1.compute.internal>
2019-01-31 07:16:25 +0000

    test

--- myAppNG/app.js
+++ myAppNG/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("app",[]);
 app.controller("ctrl",["$scope","$http", function($scope,$http){
     $scope.name="Hello";
-    $http.get('/',{}).then(function(res){
+    $http.get('/api/',{}).then(function(res){
         console.log("response");
     },function(err){
         console.log("response");
