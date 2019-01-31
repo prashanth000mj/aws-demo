b725f16c21065f43dcd1f67b09b8f2214742235c
Prashanth MJ <prashanth000mj@gmail.com>
2019-01-31 14:00:11 +0530

    test

--- myAppNG/app.js
+++ myAppNG/app.js
@@ -1,7 +1,9 @@
 var app = angular.module("app",[]);
 app.controller("ctrl",["$scope","$http", function($scope,$http){
     $scope.name="Hello from Angular JS (UI)";
+    $scope.msg="";
     $http({method:'GET',url:'/api/',transformResponse: function(data, headersGetter){return {data: data};}}).then(function(res){
+        $scope.msg=res.data.data;
         console.log("response");
     },function(err){
         console.log("response");
